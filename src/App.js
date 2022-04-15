f927737846b041fc3a3670863adfb04fc69d8c8e
Eltac E. Malikmammadov <Eltacem@code.local>
2022-04-15 17:47:39 +0400

    'com-1'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 
 function App() {
+
+  const [data, setData] = useState(undefined);
+  const [PFinp,setPFInp] = useState("")
+
+  useEffect((() => {
+    axios.get("https://api.npoint.io/20c1afef1661881ddc9c")
+      .then(resp =>
+        setData(resp.data)
+        )
+      .catch(err => console.log(err))
+  }), [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <input className="inp" onChange={(e)=> setPFInp(e.target.value)} type="text"/>
+
+
+        <div className="cards">
+        {data == undefined ? console.log("404") :
+        
+        data.playerList.sort((x,y)=>
+        {
+          return x.Value - y.Value
+        }).filter((e)=>
+        {
+         if (PFinp === "")
+         {
+          return e  
+         }
+         else if(e.PFName.toLowerCase().includes(PFinp.toLocaleLowerCase()) || e.TName.toLowerCase().includes(PFinp.toLocaleLowerCase()))
+         {
+          return e
+         }
+        }).map((e,i)=>
+        {
+          return(
+            
+          <div key={i} className="card">
+          <img className="card-img-top" src={`../img/${e.Id}.jpg`} alt="Card image cap" />
+          <div className="card-body">
+            <p className="card-title">{e.PFName}</p>
+            <p className="card-text">{e.SkillDesc}</p>
+            <p className="card-text">{e.Value} Million $</p>
+          </div>
+         </div>
+          )
+        })
+        
+        }
+        
+        </div>
+
+
+
+
     </div>
   );
 }
